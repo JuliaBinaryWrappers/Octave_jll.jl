4eab60afc6fdc52d965e949ba370489355139863
jlbuild <juliabuildbot@gmail.com>
2024-11-27 21:11:11 +0000

    Octave_jll build 9.2.0+0

--- src/wrappers/powerpc64le-linux-gnu-cxx03.jl
+++ src/wrappers/powerpc64le-linux-gnu-cxx03.jl
@@ -2,10 +2,11 @@
 export octave
 
 using CompilerSupportLibraries_jll
-using OpenBLAS32_jll
+using libblastrampoline_jll
 using SuiteSparse32_jll
 using Arpack32_jll
 using Sundials32_jll
+using QRupdate_ng_jll
 using CXSparse_jll
 using PCRE2_jll
 using Readline_jll
@@ -24,7 +25,7 @@ using GraphicsMagick_jll
 JLLWrappers.@generate_wrapper_header("Octave")
 JLLWrappers.@declare_executable_product(octave)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, OpenBLAS32_jll, SuiteSparse32_jll, Arpack32_jll, Sundials32_jll, CXSparse_jll, PCRE2_jll, Readline_jll, Libiconv_jll, Zlib_jll, Bzip2_jll, FFTW_jll, GLPK_jll, GMP_jll, LibCURL_jll, Qhull_jll, HDF5_jll, rapidjson_jll, libsndfile_jll, GraphicsMagick_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, libblastrampoline_jll, SuiteSparse32_jll, Arpack32_jll, Sundials32_jll, QRupdate_ng_jll, CXSparse_jll, PCRE2_jll, Readline_jll, Libiconv_jll, Zlib_jll, Bzip2_jll, FFTW_jll, GLPK_jll, GMP_jll, LibCURL_jll, Qhull_jll, HDF5_jll, rapidjson_jll, libsndfile_jll, GraphicsMagick_jll)
     JLLWrappers.@init_executable_product(
         octave,
         "bin/octave",
